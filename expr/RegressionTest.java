bb9ac7fe10a6cb9ebf41278dd98eb96dddd3cda4
darius <darius@96-41-123-222.dhcp.mtpk.ca.charter.com>
2010-02-12 01:05:52 -0800

    add test cases from benchmark.sh

--- expr/RegressionTest.java
+++ expr/RegressionTest.java
@@ -65,7 +65,7 @@ public class RegressionTest {
 	expect(-3.0 * Math.pow(1.01, 100.1), "  -3 * 1.01^100.1  ");
 
 	Variable x = Variable.make("x");
-	x.setValue(-40);
+	x.setValue(-40.0);
 	expect(-171.375208, "-0.00504238 * x^2 + 2.34528 * x - 69.4962");
 
         {
@@ -80,6 +80,28 @@ public class RegressionTest {
             if (!caught)
                 throw new Error("Test failed: unknown variable allowed");
         }
+
+        x.setValue(1.1);
+
+        expect(137,                  "137");
+        expect(Math.PI,              "pi");
+        expect(1.1,                  "x");
+        expect(3.8013239000000003,   "3.14159 * x^2");
+        expect(-1.457526100326025,   "sin(10*x) + sin(9*x)");
+        expect(0.8907649332805846,   "sin(x) + sin(100*x)/100");
+        expect(-0.16000473871962462, "sin(0.1*x) * (sin(9*x) + sin(10*x))");
+        expect(0.29819727942988733,  "exp(-x^2)");
+        expect(0.43226861565393254,  "2^(-x^2)");
+        expect(0.7075295010833899,   "(x^3)^(-x^2)");
+        expect(0.8678400091286832,   "x*sin(1/x)");
+        expect(-5.89,                "x^2-x-6");
+        expect(3.1953090617340916,   "sqrt(3^2 + x^2)");
+        expect(1.3542460218188073,   "atan(5/x)");
+        expect(1.5761904761904764,   "(x^2 + x + 1)/(x + 1)");
+        expect(2.6451713395638627,   "(x^3 - (4*x^2) + 12)/(x^2 + 2)");
+        expect(-2.2199999999999998,  "-2*(x-3)^2+5");
+        expect(1.2000000000000002,   "2*abs(x+1)-3");
+        expect(2.7910571473905725,   "sqrt(9-x^2)");
 
 	System.out.println("All tests passed.");
     }
